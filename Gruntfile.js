433a9a796e8e17e9fc17054debea07cb972c8b17
nicholsd <nicholsd@opentext.com>
2016-08-12 10:12:37 -0400

    Add logos and grunt-spritesmit to create a sprite

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+var config = function (grunt) {
+  
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    clean: {
+      production: ['dist/*']
+    },
+
+    // Spritesmith: create a sprite and corresponding css automagically
+  
+    sprite:{
+      logos: {
+        src: 'src/assets/images/logos/*.png',
+        dest: 'src/assets/images/logos.png',
+        destCss: 'src/assets/style/logos.css',
+        imgOpts: { quality: 100 }
+      }
+    }
+  });
+  
+  // Load tasks
+   
+  grunt.loadNpmTasks('grunt-spritesmith');
+
+  // Register custom tasks
+  
+  grunt.registerTask('build-sprites', ['sprite:logos'])
+};
+
+module.exports = config;
