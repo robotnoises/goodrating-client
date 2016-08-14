adb33f1efcab15f43edd31a5c4b9552393fe58a5
David Nichols <davenich@gmail.com>
2016-08-13 22:08:55 -0400

    Re-serialize querystring

--- src/modules/table/controllers/tableController.js
+++ src/modules/table/controllers/tableController.js
@@ -4,16 +4,16 @@
   
   angular.module('goodrating.table')
   
-  .controller('tableController', ['$scope', '$routeParams', 'tableDataService', tableController]);
+  .controller('tableController', ['$scope', '$routeParams', '$location', 'tableDataService', tableController]);
 
-  function tableController($scope, $routeParams, tableDataService) {
+  function tableController($scope, $routeParams, $location, tableDataService) {
     
     $scope.ratings = [];
     $scope.loaded = false;
 
     var year = $routeParams.year || '';
 
-    tableDataService.calc('/calc/' + year)
+    tableDataService.calc('/calc/' + year, $location.search())
       .then(function (data) {
         $scope.ratings = data;
         $scope.loaded = !!data;
--- src/modules/table/services/tableDataService.js
+++ src/modules/table/services/tableDataService.js
@@ -4,17 +4,18 @@
 
   angular.module('goodrating.table')
 
-  .factory('tableDataService', ['$http', '$q', function ($http, $q) {
+  .factory('tableDataService', ['$http', '$httpParamSerializer', '$q', 
+  function ($http, $httpParamSerializer, $q) {
 
     var _dataService = {};
     
     var apiBase = 'https://goodrating.herokuapp.com/api/v1'; // 'http://localhost:12345/api/v1';
 
-    function _calc(path) {
+    function _calc(path, query) {
       return $q(function (resolve, reject) {
         $http({
           method: 'GET',
-          url: apiBase + path
+          url: apiBase + path + '?' + $httpParamSerializer(query)
         })
         .then(function (response) {
           resolve(response.data);
