4c2a797bafaa7277bcecf92b5ecd65c96aa6a540
David Nichols <davenich@gmail.com>
2016-08-15 15:45:31 -0400

    Sort them columns

--- src/modules/table/controllers/tableController.js
+++ src/modules/table/controllers/tableController.js
@@ -8,8 +8,30 @@
 
   function tableController($scope, $routeParams, $location, tableDataService) {
     
+    var COLUMN = {
+      'WIN_PERCENTAGE': 'win_percentage',
+      'YPP_OFFENSE': 'ypp_offense',
+      'YPP_DEFENSE': 'ypp_defense',
+      'RECRUITING_SCORE': 'recruiting_score',
+      'ATS': 'ats',
+      'WIN_PERCENTAGE_RATING': 'win_percentage_rating',
+      'YPP_OFFENSE_RATING': 'ypp_offense_rating',
+      'YPP_DEFENSE_RATING': 'ypp_defense_rating',
+      'RECRUITING_SCORE_RATING': 'recruiting_score_rating',
+      'TOTAL_RATING': 'total_rating'
+    };
+
     $scope.ratings = [];
     $scope.loaded = false;
+    $scope.sortColumn = ($location.search().sortby) ? COLUMN[$location.search().sortby.toUpperCase()] : COLUMN.TOTAL_RATING;
+
+    // Scope methods
+
+    $scope.sortBy = function (column) {
+      $location.search('sortby', COLUMN[column.toUpperCase()] || COLUMN.TOTAL_RATING)
+    }
+
+    // Init
 
     var year = $routeParams.year || '';
 
